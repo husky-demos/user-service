8316e7fd6c06469a2ccce1e812cc06175b2b3086
Husky <husky.robot.dog@gmail.com>
2022-02-27 19:34:14 +0800

    init

--- service/src/main/java/org/example/Main.java
+++ service/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.ServerBuilder;
 import org.example.service.UserService;
 
@@ -7,10 +9,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        ManagedChannel walletServiceChannel = ManagedChannelBuilder.forTarget("localhost:9000").usePlaintext().build();
+
         final var server = ServerBuilder.forPort(9001)
-                .addService(new UserService())
+                .addService(new UserService(walletServiceChannel))
                 .build()
                 .start();
+
         server.awaitTermination();
     }
 }
--- service/src/main/java/org/example/service/UserService.java
+++ service/src/main/java/org/example/service/UserService.java
@@ -1,6 +1,39 @@
 package org.example.service;
 
+import com.github.husky_demos.user_service.v1.UserModel;
 import com.github.husky_demos.user_service.v1.UserServiceGrpc;
+import com.github.husky_demos.wallet_service.v1.WalletModel;
+import com.github.husky_demos.wallet_service.v1.WalletServiceGrpc;
+import io.grpc.Channel;
+import io.grpc.stub.StreamObserver;
 
 public class UserService extends UserServiceGrpc.UserServiceImplBase {
+
+    private final WalletServiceGrpc.WalletServiceBlockingStub walletService;
+
+    public UserService(Channel walletServiceChannel) {
+        walletService = WalletServiceGrpc.newBlockingStub(walletServiceChannel);
+    }
+
+
+    @Override
+    public void login(UserModel.LoginRequest request, StreamObserver<UserModel.LoginResult> responseObserver) {
+
+        WalletModel.Wallet wallet = walletService.queryByUserId(WalletModel.Id.newBuilder().build());
+        System.out.println(wallet.toString());
+
+        UserModel.LoginResult result = UserModel.LoginResult.newBuilder()
+                .setUser(UserModel.User.newBuilder()
+                        .setId("123")
+                        .setNickName("husky")
+                        .setLoginName("abc")
+                        .setLoginPass("666")
+                        .setIsLocking(false)
+                        .build())
+                .setToken("AAABBBCCC666")
+                .setExpireTime(System.currentTimeMillis())
+                .build();
+        responseObserver.onNext(result);
+        responseObserver.onCompleted();
+    }
 }
